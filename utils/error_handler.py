6da7bf1f1117dc5c089e9cecbdb56e5949685c9a
Avellea Jenkins <dynamicdonkey0@gmail.com>
2021-08-19 14:26:54 -0500

    Changed error handling a bit, hopefully this works

--- utils/error_handler.py
+++ utils/error_handler.py
@@ -1,8 +1,24 @@
+import discord
 from discord.ext import commands
 
 
 class CommandErrorHandler(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
+        error_embed = discord.Embed(
+            title="Error",
+            color=0xE94D4E
+        )
         if isinstance(error, commands.NSFWChannelRequired):
-            await ctx.send(f"{ctx.command} is disabled in non-NSFW channels.")
+            error_embed.add_field(
+                name="NSFW content",
+                value=f"{ctx.command} is disabled in non-NSFW channels."
+            )
+            error_embed.set_image(
+                url="https://raw.githubusercontent.com/DynamicDonkey/Mayuko/master/assets/nsfw_error.png"
+            )
+            error_embed.set_footer(
+                text="Mayuko",
+                icon_url="https://raw.githubusercontent.com/DynamicDonkey/Mayuko/master/assets/pfp.jpg",
+            )
+            await ctx.send(embed=error_embed)
